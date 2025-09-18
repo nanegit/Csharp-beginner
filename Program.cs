e8dfff5439a9fbd3cc37ceaaf4d2a4b7d7821d2c
nanegit <filesnane@gmail.com>
2025-09-18 22:05:43 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+///////Laborator 1.1
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Hello World, C#!");
+//    }
+//}
+
+
+////////Laborator 1.2
+
+//class InpOut
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("How old are you?");
+//        Console.Write("Age: ");
+//        string age = Console.ReadLine();
+//        Console.WriteLine("What is your name?");
+//        Console.Write("Name: ");
+//        string name = Console.ReadLine();
+//        Console.WriteLine("Hello " + name + "!");
+//        Console.WriteLine("You are " + age + " years old.");
+//    }
+//}
+
+
+/////Laborator 1.3
+//class Variables
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("input first number:");
+//        int FirsNum = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine("input second number:");
+//        int SecNum = Convert.ToInt32(Console.ReadLine());
+//        int sum = FirsNum + SecNum;
+//        Console.WriteLine("The sum of " + FirsNum + " and " + SecNum + " is: " + sum);
+//    }
+//}
+
+
+
+///// lab 1 homework 1
+///
+//class Program
+//{
+//    static void Main(string[] args)
+//    { 
+//      Console.WriteLine("Enter your name: ");
+//      string name = Console.ReadLine()!;
+//      Console.WriteLine("Enter your last name: ");
+//      string Lastname = Console.ReadLine()!;
+//      Console.WriteLine("Enter your city: ");
+//      string city = Console.ReadLine()!;
+//      Console.WriteLine("Hello,I am " + name +" "+ Lastname+ ", I am living in " + city);
+//    }
+//}
+
+
+////lab 1 homework 2
+
+//class Variables
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("input first number:");
+//        int FirsNum = Convert.ToInt32(Console.ReadLine());
+
+//        Console.WriteLine("input second number:");
+//        int SecNum = Convert.ToInt32(Console.ReadLine());
+//        int sum = FirsNum + SecNum;
+//        Console.WriteLine("The sum of " + FirsNum + " and " + SecNum + " is: " + sum);
+//        int dif = FirsNum - SecNum;
+//        Console.WriteLine("The difference of " + FirsNum + " and " + SecNum + " is: " + dif);
+//        int prod = FirsNum * SecNum;
+//        Console.WriteLine("The product of " + FirsNum + " and " + SecNum + " is: " + prod);
+//        if (SecNum != 0)
+//        {
+//            int quot = FirsNum / SecNum;
+//            Console.WriteLine("The quotient of " + FirsNum + " and " + SecNum + " is: " + quot);
+//        }
+//        else
+//        {
+//            Console.WriteLine("Division by zero is not allowed.");
+//        }
+//    }
+//}
+
+
+//lab 1 homework 3
+
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("*****");
+        Console.WriteLine("*   *");
+        Console.WriteLine("*****");
+    }
+}
