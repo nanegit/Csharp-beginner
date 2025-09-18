7fcda8801754f9fc64c71c98b8453a87033cba2e
nanegit <filesnane@gmail.com>
2025-09-18 22:07:10 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+//////Laborator 2.2
+//class Age
+//{
+//    static void Main()
+//    {
+//        Console.WriteLine("Enter your age: ");
+//        int age = Convert.ToInt32(Console.ReadLine());
+//        if (age >= 18)
+//        {
+//            Console.WriteLine("you are an adult");
+//        }
+//        else { Console.WriteLine("YOU ARE A MINOR"); }
+
+//    }
+//}
+
+
+////Laborator 2.3
+///
+
+//class Boologic {
+//    static void Main(string[] args)
+//    {
+//        Console.Write("Do you have ID?: ");
+//        bool ID = Convert.ToBoolean(Console.ReadLine());
+//        Console.Write("Do you have ticket?: ");
+//        bool ticket = Convert.ToBoolean(Console.ReadLine());
+//        if(ID && ticket)
+//        {
+//            Console.WriteLine("Welcome!");
+//        }
+//        else
+//        {
+//            Console.WriteLine("access is denied");
+//        }
+
+//    }
+//}
+
+
+////Laborator 2 homework 1
+
+
+
+/////laborator 2 homework 2
+
+//class AreaOfCircle
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.Write("Radius: ");
+//        double R = Convert.ToDouble(Console.ReadLine());
+//        double s = double.Pi * R * R;
+//        Console.WriteLine("Area: " + s);
+//    }
+//}
+
+
+/////laborator 2 homework 3
+
+//class OddOrEven
+//{
+//    static void Main(string[] args)
+//    {
+//        Console.WriteLine("Input number: ");
+//        int num = Convert.ToInt32(Console.ReadLine());
+//        if (num <= 0)
+//        {
+//            Console.WriteLine("It is neither odd nor even.");
+
+//        }
+//        else if(num % 2 == 0)
+//        {
+//            Console.WriteLine("Num is even ");
+
+//        }
+//        else
+//        {
+//            Console.WriteLine("Num is odd");
+//        }
+//    }
+//}
+
+
